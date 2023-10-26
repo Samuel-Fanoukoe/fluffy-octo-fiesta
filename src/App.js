be5054c29c94769342e6db4795e4f8b495c5beb7
Samuel-Fanoukoe <samuel.fanoukoe@dataroots.io>
2023-10-26 14:03:47 +0200

    first push.. lol

--- src/App.js
+++ src/App.js
@@ -7,7 +7,8 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+           Hi there /n
+           you too like trouble.. yeye boyy
         </p>
         <a
           className="App-link"
